e9a1eca4dd179eed94867c49d454fea0b647982d
Gena <mr.genag@mail.ru>
2022-08-03 16:45:05 +0500

    Added index.html and main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+const products = [
+    {
+        id: 1,
+        title: 'Ноутбук',
+        price: 20000
+    },
+    {
+        id: 1,
+        title: 'Ноутбук',
+        price: 20000
+    },
+    {
+        id: 1,
+        title: 'Ноутбук',
+        price: 20000
+    },
+    {
+        id: 1,
+        title: 'Ноутбук',
+        price: 20000
+    },
+]
+
+const renderPage = list => {
+    const productsList = list.map()
+}
+
+renderPage(products);
