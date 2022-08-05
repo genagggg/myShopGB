eb01fb89f2e07358589faa0a630e9880eecc7ec0
Gena <mr.genag@mail.ru>
2022-08-05 18:08:38 +0500

    Lessons1 resolved

--- main.js
+++ main.js
@@ -25,17 +25,17 @@ const products = [
     },
 ]
 
-const renderProduct = (title, price, img) => `
+const renderProduct = (products) => `
 <div class = "product-item">
-<img class="product-item-images" src="images/${img}">
-<h3 class="product-name"> Название товара: ${title}</h3>
-<p class="product-price"> Цена товара: ${price}</p>
+<img class="product-item-images" src="images/${products.img}">
+<h3 class="product-name"> Название товара: ${products.title}</h3>
+<p class="product-price"> Цена товара: ${products.price}</p>
 <div class="button-addet-busket">Добавить</div>
 </div>
 `
 
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price, item.img))
+    const productsList = list.map(item => renderProduct(item))
 
     document.querySelector('.products').innerHTML = productsList.join('');
 }
